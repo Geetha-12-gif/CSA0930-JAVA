0c6fb0c3f794b6cf02aa7c75b4bcac362530df33
Geetha-12-gif <113919199+Geetha-12-gif@users.noreply.github.com>
2024-03-18 12:18:22 +0530

    Add files via upload

---
+++ PrimitiveDataTypesDemo.java
@@ -0,0 +1,29 @@
+public class PrimitiveDataTypesDemo {
+    public static void main(String[] args) {
+        // Integer data types: byte, short, int, and long
+        byte byteVar = 100;
+        short shortVar = 1000;
+        int intVar = 100000;
+        long longVar = 1000000000L;
+
+        // Floating-point data types: float and double
+        float floatVar = 10.5f;
+        double doubleVar = 10.56789;
+
+        // Character data type: char
+        char charVar = 'A';
+
+        // Boolean data type: boolean
+        boolean boolVar = true;
+
+        // Printing the values of each primitive data type
+        System.out.println("Byte variable: " + byteVar);
+        System.out.println("Short variable: " + shortVar);
+        System.out.println("Int variable: " + intVar);
+        System.out.println("Long variable: " + longVar);
+        System.out.println("Float variable: " + floatVar);
+        System.out.println("Double variable: " + doubleVar);
+        System.out.println("Char variable: " + charVar);
+        System.out.println("Boolean variable: " + boolVar);
+    }
+}
